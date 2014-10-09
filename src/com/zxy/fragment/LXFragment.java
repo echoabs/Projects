5604626b32d28c9a26645ecabdd4b5e2da465f1e
Administrator <Administrator@PH6KH2ZGYKQF7QC>
2014-10-09 22:34:48 +0800

    多个地方

--- src/com/zxy/fragment/LXFragment.java
+++ src/com/zxy/fragment/LXFragment.java
@@ -13,7 +13,7 @@ public class LXFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		TextView tv_title = new TextView(getActivity());
-		tv_title.setText("我操你老母");
+		tv_title.setText("我操你老母到底怎么搞的啊");
 		return tv_title;
 	}
 
