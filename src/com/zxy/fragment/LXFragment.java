6dc92fd89f4943cd3726b5a06e2873969c3dab97
echoabs <278489710@qq.com>
2014-10-09 21:44:02 +0800

    框架再次更新

--- src/com/zxy/fragment/LXFragment.java
+++ src/com/zxy/fragment/LXFragment.java
@@ -13,7 +13,7 @@ public class LXFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		TextView tv_title = new TextView(getActivity());
-		tv_title.setText("更改以后");
+		tv_title.setText("再次更新以后");
 		return tv_title;
 	}
 
